218d70ad83b0882679e0a4c4d7a71cdc73f5dbf8
JalzDeLezZ <j4mes.delez@gmail.com>
2022-08-27 15:24:48 -0500

    65. completing the migation to the services :: change of themes

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,35 +7,16 @@ import { SettingsService } from 'src/app/services/settings.service';
   styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
-  public tagTheme_header = document.querySelector('#theme');
-  public tagsTheme: NodeListOf<Element> =
-    document.querySelectorAll('.selector');
 
   constructor(
     private _settingsService: SettingsService
   ) {}
 
   ngOnInit(): void {
-    this.tagsTheme = document.querySelectorAll('.selector');
-    this.mCheckCurrentTheme();
+    this._settingsService.act_CheckCurrentTheme();
   }
 
   changeTheme(pTheme: string) {
     this._settingsService.act_changeTheme(pTheme);
-    this.mCheckCurrentTheme();
-  }
-
-  mCheckCurrentTheme() {
-    this.tagsTheme.forEach((pI) => {
-      pI.classList.remove('working');
-
-      const tag_a = pI.getAttribute('data-theme'); // get value of data-theme
-      const urlSelected = `./assets/css/colors/${tag_a}.css`;
-      const currentTheme = this.tagTheme_header?.getAttribute('href');
-
-      if (urlSelected === currentTheme) {
-        pI.classList.add('working');
-      }
-    });
   }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -4,20 +4,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class SettingsService {
-
   private tagTheme_header = document.querySelector('#theme');
 
   constructor() {
-
-    const url = localStorage.getItem('theme') || './assets/css/colors/megna-dark.css';
+    const url =
+      localStorage.getItem('theme') || './assets/css/colors/megna-dark.css';
     this.tagTheme_header?.setAttribute('href', url);
   }
-
 
   act_changeTheme(pTheme: string) {
     const url = `./assets/css/colors/${pTheme}.css`;
     this.tagTheme_header?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.act_CheckCurrentTheme();
   }
 
+  act_CheckCurrentTheme() {
+    const tagsTheme : NodeListOf<Element> = document.querySelectorAll('.selector');
+
+    tagsTheme.forEach((pI) => {
+      pI.classList.remove('working');
+
+      const tag_a = pI.getAttribute('data-theme'); // get value of data-theme
+      const urlSelected = `./assets/css/colors/${tag_a}.css`;
+      const currentTheme = this.tagTheme_header?.getAttribute('href');
+
+      if (urlSelected === currentTheme) {
+        pI.classList.add('working');
+      }
+    });
+  }
 }
